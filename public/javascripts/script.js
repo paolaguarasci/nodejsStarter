f06832d340c21adbcf62b33f71604e8bb65ed316
Paola Guarasci <paolaguarasci@gmail.com>
2019-05-25 15:26:00 +0200

    ok

---
+++ public/javascripts/script.js
@@ -0,0 +1,5 @@
+const main = document.querySelector(".main");
+const txt = document.createTextNode("Hello Express!!!");
+const h1 = document.createElement("h1");
+h1.appendChild(txt);
+main.appendChild(h1);
